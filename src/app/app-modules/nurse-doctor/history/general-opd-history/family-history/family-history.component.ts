5994a4289b7ee342b8167c2222742254a6bf6a04
Bhallamudi Pratibha <BH20131883@wipro.com>
2024-05-15 17:28:24 +0530

    fixed enabling and disabling for the family history

--- src/app/app-modules/nurse-doctor/history/general-opd-history/family-history/family-history.component.ts
+++ src/app/app-modules/nurse-doctor/history/general-opd-history/family-history/family-history.component.ts
@@ -185,7 +185,16 @@ export class FamilyHistoryComponent implements OnInit, DoCheck, OnDestroy {
         const k: any = formArray.get('' + i);
         k.patchValue(temp[i]);
         k.markAsTouched();
+        k.markAsDirty();
         this.filterFamilyDiseaseList(temp[i].diseaseType, i);
+
+        if (
+          k?.get('diseaseType')?.value !== null &&
+          k?.get('diseaseType')?.value !== 'None' &&
+          k?.get('diseaseType')?.value !== 'Nil'
+        ) {
+          k?.get('familyMembers')?.enable();
+        }
       }
 
       if (i + 1 < temp.length) this.addFamilyDisease();
@@ -253,6 +262,17 @@ export class FamilyHistoryComponent implements OnInit, DoCheck, OnDestroy {
     });
 
     this.previousSelectedDiseaseList[i] = disease;
+
+    //To disable the fields
+    console.log('%%%%%%%%%%%%%%%%%%%%%%%%%%%%', disease);
+
+    if (disease.diseaseType !== 'Nil' && disease.diseaseType !== 'None') {
+      familyDiseaseForm?.get('familyMembers')?.enable();
+      familyDiseaseForm?.get('familyMembers')?.reset();
+    } else {
+      familyDiseaseForm?.get('familyMembers')?.disable();
+      familyDiseaseForm?.get('familyMembers')?.reset();
+    }
   }
 
   removeFamilyDiseaseExecptNone() {
@@ -350,7 +370,7 @@ export class FamilyHistoryComponent implements OnInit, DoCheck, OnDestroy {
       diseaseTypeID: null,
       diseaseType: null,
       otherDiseaseType: null,
-      familyMembers: null,
+      familyMembers: { value: null, disabled: true },
       snomedCode: null,
       snomedTerm: null,
     });
