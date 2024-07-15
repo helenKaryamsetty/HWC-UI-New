ca9aa4e69a2d03c295392a63675ba117ba73e967
nkokkiligadda87 <nkokkiligadda87@gmail.com>
2024-07-15 05:32:06 +0530

    CSS chnages

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -24,16 +24,16 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
-const commonIP = 'http://183.82.107.186:8080';
-const identityIP = 'http://183.82.107.186:8080';
-const tmIP = 'http://183.82.107.186:8080';
-const IP104 = 'http://183.82.107.186:8080';
-const mmuIP = 'http://183.82.107.186:8080';
-const schedulerIP = 'http://183.82.107.186:8080';
-const adminIP = 'http://183.82.107.186:8080';
-const inventoryUI_IP = 'http://183.82.107.186:8080';
+const commonIP = 'https://amritwprdev.piramalswasthya.org';
+const identityIP = 'https://amritwprdev.piramalswasthya.org';
+const tmIP = 'https://amritwprdev.piramalswasthya.org';
+const IP104 = 'https://amritwprdev.piramalswasthya.org';
+const mmuIP = 'https://amritwprdev.piramalswasthya.org';
+const schedulerIP = 'https://amritwprdev.piramalswasthya.org';
+const adminIP = 'https://amritwprdev.piramalswasthya.org';
+const inventoryUI_IP = 'https://amritwprdev.piramalswasthya.org';
 
-const FHIRIP = 'http://183.82.107.186:8080';
+const FHIRIP = 'https://amritwprdev.piramalswasthya.org';
 const SERVER_IP = '10.208.122.39';
 const SWYMED_IP = '14.143.13.109';
 
