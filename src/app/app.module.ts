9d8321472aa9bcec5bdf59d6dc230e6dbe21046b
nkokkiligadda87 <nkokkiligadda87@gmail.com>
2024-08-07 19:17:47 +0530

    CSS changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -52,7 +52,7 @@ import { CbacService } from './app-modules/nurse-doctor/shared/services/cbac.ser
 import { HrpService } from './app-modules/nurse-doctor/shared/services/hrp.service';
 import { FamilyTaggingService } from './app-modules/registrar/shared/services/familytagging.service';
 import { RegistrarService } from './app-modules/registrar/shared/services/registrar.service';
-import { RegistrationModule } from 'Common-UI/srcs/registrar/registration.module';
+import { RegistrationModule } from 'Common-UI/src/registrar/registration.module';
 import { SharedModule } from './app-modules/core/components/shared/shared.module';
 import { RegistrarModule } from './app-modules/registrar/registrar.module';
 
