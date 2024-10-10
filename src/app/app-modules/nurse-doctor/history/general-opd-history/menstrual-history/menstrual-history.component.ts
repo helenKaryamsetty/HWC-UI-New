6eb5bf8a3b527de6ad246218a0e4f58358948df3
devikasuresh20 <devika.s36@wipro.com>
2024-10-10 17:06:14 +0530

    ANC menstrual history changes

--- src/app/app-modules/nurse-doctor/history/general-opd-history/menstrual-history/menstrual-history.component.ts
+++ src/app/app-modules/nurse-doctor/history/general-opd-history/menstrual-history/menstrual-history.component.ts
@@ -150,6 +150,9 @@ export class MenstrualHistoryComponent implements OnInit, DoCheck, OnDestroy {
         return item.name === temp;
       })[0];
       this.menstrualHistoryForm.patchValue({ menstrualCycleStatus: temp });
+      this.menstrualHistoryForm.get('lMPDate')?.disable();
+    } else {
+      this.menstrualHistoryForm.get('lMPDate')?.enable();
     }
   }
 
